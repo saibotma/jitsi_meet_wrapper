c538ac2efced2ac5bf52ce0aab9d11e82f5e2bfd
Tobias Marschall <tobias.marschall@online.de>
2021-11-21 20:58:08 +0100

    add android implementation

--- android/src/main/kotlin/dev/saibotma/jitsi_meet_wrapper/JitsiMeetWrapperPlugin.kt
+++ android/src/main/kotlin/dev/saibotma/jitsi_meet_wrapper/JitsiMeetWrapperPlugin.kt
@@ -1,35 +1,140 @@
 package dev.saibotma.jitsi_meet_wrapper
 
+import android.app.Activity
+import android.content.Intent
+import android.util.Log
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
+const val JITSI_PLUGIN_TAG = "JITSI_MEET_PLUGIN"
+const val JITSI_METHOD_CHANNEL = "jitsi_meet"
+const val JITSI_EVENT_CHANNEL = "jitsi_meet_events"
+const val JITSI_MEETING_CLOSE = "JITSI_MEETING_CLOSE"
+
 /** JitsiMeetWrapperPlugin */
-class JitsiMeetWrapperPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class JitsiMeetWrapperPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "jitsi_meet_wrapper")
-    channel.setMethodCallHandler(this)
-  }
+    private var activity: Activity? = null
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    constructor(activity: Activity) : this() {
+        this.activity = activity
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, JITSI_METHOD_CHANNEL)
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        Log.d(JITSI_PLUGIN_TAG, "method: ${call.method}")
+        Log.d(JITSI_PLUGIN_TAG, "arguments: ${call.arguments}")
+
+        when (call.method) {
+            "joinMeeting" -> {
+                joinMeeting(call, result)
+            }
+            "closeMeeting" -> {
+                closeMeeting(call, result)
+            }
+            else -> result.notImplemented()
+        }
+    }
+
+    private fun joinMeeting(call: MethodCall, result: Result) {
+        val room = call.argument<String>("room")
+        if (room.isNullOrBlank()) {
+            result.error("400",
+                    "room can not be null or empty",
+                    "room can not be null or empty")
+            return
+        }
+
+        Log.d(JITSI_PLUGIN_TAG, "Joining Room: $room")
+
+        val userInfo = JitsiMeetUserInfo()
+        userInfo.displayName = call.argument("userDisplayName")
+        userInfo.email = call.argument("userEmail")
+        if (call.argument<String?>("userAvatarURL") != null) {
+            userInfo.avatar = URL(call.argument("userAvatarURL"))
+        }
+
+        var serverURLString = call.argument<String>("serverURL")
+        if (serverURLString == null) {
+            serverURLString = "https://meet.jit.si";
+        }
+        val serverURL = URL(serverURLString)
+        Log.d(JITSI_PLUGIN_TAG, "Server URL: $serverURL, $serverURLString")
+
+        val optionsBuilder = JitsiMeetConferenceOptions.Builder()
+
+        // Set meeting options
+        optionsBuilder
+                .setServerURL(serverURL)
+                .setRoom(room)
+                .setSubject(call.argument("subject"))
+                .setToken(call.argument("token"))
+                .setAudioMuted(call.argument("audioMuted") ?: false)
+                .setAudioOnly(call.argument("audioOnly") ?: false)
+                .setVideoMuted(call.argument("videoMuted") ?: false)
+                .setUserInfo(userInfo)
+
+        // Add feature flags into options, reading given Map
+        if (call.argument<HashMap<String, Any>?>("featureFlags") != null) {
+            val featureFlags = call.argument<HashMap<String, Any>>("featureFlags")
+            featureFlags!!.forEach { (key, value) ->
+                if (value is Boolean) {
+                    val boolVal = value.toString().toBoolean()
+                    optionsBuilder.setFeatureFlag(key, boolVal)
+                } else {
+                    val intVal = value.toString().toInt()
+                    optionsBuilder.setFeatureFlag(key, intVal)
+                }
+            }
+        }
+
+        // Build with meeting options and feature flags
+        val options = optionsBuilder.build()
+
+        JitsiMeetWrapperPluginActivity.launchActivity(activity, options)
+        result.success("Successfully joined room: $room")
+    }
+
+    private fun closeMeeting(call: MethodCall, result: Result) {
+        val intent = Intent(JITSI_MEETING_CLOSE)
+        activity?.sendBroadcast(intent)
+        result.success(null)
+    }
+
+    override fun onDetachedFromActivity() {
+        this.activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        onAttachedToActivity(binding)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        this.activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity()
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
